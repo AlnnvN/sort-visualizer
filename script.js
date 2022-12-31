3f51a4f97881d6ae7c7daf9947c1ac485bc753b9
alnnvn <alanvan3791@gmail.com>
2022-12-31 18:24:30 -0300

    created classes for settings and canvas

--- script.js
+++ script.js
@@ -1,158 +1,248 @@
+/*
 const canvas = createCanvas();
-const ctx = canvas.getContext('2d');
+const CTX = canvas.getContext('2d');
 
-//#001011
+//#001011*/
+class Settings {
+    constructor(method, order, speed, quantity) {
+        this.method = method;
+        this.order = order;
+        this.speed = speed;
+        this.quantity = quantity;
+    }
 
-class Column{
-    constructor(x, value){
-        let gapMultiplier = unsortedArray.length/(unsortedArray.length*5)**2
-        this.gap = (canvas.width)*gapMultiplier;
-        this.x = x;
+    switchSortMethod() {
+        return new BubbleSort(this.order);
+    }
+}
+
+class Canvas {
+    constructor(color) {
+        this.element = document.getElementById('canvas');
+        this.context = document.getElementById('canvas').getContext('2d');
+        this.ratio = Math.ceil(window.devicePixelRatio);
+        this.setup();
+
+        //global variables
+        this.height = this.element.height;
+        this.width = this.element.width;
+
+        //inner settings
+        this.sideMargin = this.element.width * 0.05;
+        this.topMargin = this.element.height * 0.95;
+        this.bottomMargin = this.element.height * 0.02;
+
+
+        this.color = color
+    }
+
+    setup() {
+        self = this;
+        adjustFromPixelRatio();
+        limitCanvasSize();
+        addScreenInput();
+
+
+        function addScreenInput() {
+            //click anywhere on the screen to start sorting
+            self.element.addEventListener('click', sortNumberArray);
+            return;
+        }
+
+        function limitCanvasSize() {
+            //limiting the canvas size on the screen with css
+            self.element.style.width = window.innerWidth + 'px';
+            self.element.style.height = window.innerHeight + 'px';
+            return;
+        }
+
+        function adjustFromPixelRatio() {
+            //adjusting W/H proportionally to the pixel ratio with html
+            self.element.width = window.innerWidth * self.ratio;
+            self.element.height = window.innerHeight * self.ratio;
+            return;
+        }
+
+        return;
+    }
+
+    reset() {
+        let self = this
+
+        clearScreen();
+        drawBackground('#EF2D56');
+
+        function clearScreen() {
+            self.context.clearRect(0, 0, self.element.width, self.element.height);
+        }
+
+        function drawBackground(color) {
+            self.context.fillStyle = color;
+            self.context.fillRect(0, 0, self.element.width, self.element.height);
+            return;
+        }
+
+        return;
+    }
+}
+
+class Column {
+    constructor(xPos, value) {
+        let gapMultiplier = unsortedArray.length / (unsortedArray.length * 5) ** 2
+        this.gap = (canvas.width) * gapMultiplier;
+        this.x = xPos;
         this.value = value;
-        this.width = ((canvas.width-(sideMargin*2))-(this.gap*(unsortedArray.length-1)))/unsortedArray.length
+        this.width = ((canvas.width - (canvas.sideMargin * 2)) - (this.gap * (unsortedArray.length - 1))) / unsortedArray.length
         this.height = mapNumberRange(
             value,
             0,
             unsortedArray.length,
-            canvas.height*0.15, //min column height
-            topMargin //max column height
+            canvas.height * 0.15, //min column height
+            canvas.topMargin //max column height
         );
     }
 
-    draw(){
-        ctx.fillStyle = "#FFFFFC";
-        ctx.fillRect(this.x, canvas.height-bottomMargin, this.width, -this.height);
+    draw() {
+        CTX.fillStyle = "#FFFFFC";
+        CTX.fillRect(
+            this.x,
+            (canvas.height - canvas.bottomMargin),
+            this.width,
+            -this.height
+        );
         return;
     }
 
-    static getGap(){
+    static getGap() {
         return new Column().gap;
     }
 
-    static getWidth(){
+    static getWidth() {
         return new Column().width;
+    }
+
+    static getHeight() {
+        return new Column().height;
     }
 }
 
-class BubbleSort{
-    constructor(sortOrder){
-        this.sortOrder = sortOrder;
+class BubbleSort {
+    constructor(order) {
+        this.order = order
         this.i = 0;
         this.isSortedCounter = 0;
     }
 
-    sort(){
-        if(this.sortOrder === 'increasing'){
+    sort() {
+        if (this.order === 'increasing') {
             this.increasing();
             return;
         }
-        else if(this.sortOrder === 'decreasing'){
+        else if (this.order === 'decreasing') {
             this.decreasing();
             return;
         }
     }
 
-    increasing(){
+    increasing() {
         this.sortingAlgorithm(unsortedArray[this.i] > unsortedArray[this.i + 1]);
         return;
     }
 
-    decreasing(){
+    decreasing() {
         this.sortingAlgorithm(unsortedArray[this.i] < unsortedArray[this.i + 1]);
         return;
     }
 
-    sortingAlgorithm(sortCondition){
-            //sort increasing
-            if (sortCondition) {
-                let c = unsortedArray[this.i];
-                unsortedArray[this.i] = unsortedArray[this.i + 1];
-                unsortedArray[this.i + 1] = c;
-                this.isSortedCounter = 0;
-            }
-            else{
-                this.isSortedCounter++
-            }
+    sortingAlgorithm(sortCondition) {
+        //sort increasing
+        if (sortCondition) {
+            let c = unsortedArray[this.i];
+            unsortedArray[this.i] = unsortedArray[this.i + 1];
+            unsortedArray[this.i + 1] = c;
+            this.isSortedCounter = 0;
+        }
+        else {
+            this.isSortedCounter++
+        }
 
-            //check if its sorted
-            if(this.isSortedCounter >= unsortedArray.length){
-                isSorting = false; 
-            }
-    
-            this.i++; //next index
+        //check if its sorted
+        if (this.isSortedCounter >= unsortedArray.length) {
+            isSorting = false;
+        }
 
-            //if loop through the array back from the start
-            if (this.i >= unsortedArray.length - 1) {
-                this.i = 0;
-            }
+        this.i++; //next index
+
+        //if loop through the array back from the start
+        if (this.i >= unsortedArray.length - 1) {
+            this.i = 0;
+        }
     }
 }
 
-//inner settings
-var sideMargin = canvas.width*0.05;
-var topMargin = canvas.height*0.95;
-var bottomMargin = canvas.height*0.02;
 
 //global variables
+
+const canvas = new Canvas('#EF2D56');
+const CTX = canvas.context;
+const sortSettings = new Settings('bubblesort', 'increasing', 0, 3000);
+
 var unsortedArray;
 var isSorting = true;
-
-//user input / settings
 var unsortedArraySize = 1000;
-var sortOrder = 'increasing';
-var sortingSpeed = 0 //delay between operations
-var sortersQnt = 3000; //comparisons per operation
+
 
 
 start();
-//click anywhere on the screen to start sorting
-canvas.addEventListener('click', sortNumberArray);
+
+
 
 //FUNCTIONS
-function start(){
-    unsortedArray = createUnsortedArray(unsortedArraySize);    
+function start() {
+    unsortedArray = createUnsortedArray(unsortedArraySize);
     updateCanvas();
     return;
 }
 
-function updateCanvas(){
+function updateCanvas() {
     window.requestAnimationFrame(updateCanvas);
-    resetCanvas();
+    canvas.reset();
     drawGraph();
+
     return;
 }
 
-function sortNumberArray(){
-    let bubbleSort = new BubbleSort(sortOrder);
-
+function sortNumberArray() {
+    let sortMethod = sortSettings.switchSortMethod();
     var loop = function () { //infinite loop
-        let timeout = setTimeout(function () { //delay between operations
-            if(isSorting){
+        let timeout = setTimeout(() => { //delay between operations
+            if (isSorting) {
                 //define comparisons per operation
-                for(let k =0; k<sortersQnt; k++){
-                    bubbleSort.sort();  
+                for (let k = 0; k < sortSettings.quantity; k++) {
+                    sortMethod.sort();
                 }
-                
+
             }
-            else{
+            else {
                 //clear set timeouts and return
                 clearTimeout(timeout);
                 return;
             }
             //mantain loop if it doesn't return
             window.requestAnimationFrame(loop);
-        }, sortingSpeed); //delay value
+        }, sortSettings.speed); //delay value
     }
 
-    if(isSorting){
+    if (isSorting) {
         window.requestAnimationFrame(loop);
     }
+
     return;
 }
 
-function createUnsortedArray(size){
+function createUnsortedArray(size) {
     let array = [];
-    
+
     fillArray(array);
     shuffleArray(array);
 
@@ -188,57 +278,27 @@ function createUnsortedArray(size){
                 }
             })
         })
-    }  */ 
+    }  */
     return array;
 }
 
-function drawGraph(){
-    for(let i = 0; i<unsortedArray.length; i++){
-        drawColumn(
-            canvas.width*0.05+(Column.getWidth()+Column.getGap())*i,
+function drawGraph() {
+    for (let i = 0; i < unsortedArray.length; i++) {
+        let column = new Column(
+            canvas.width * 0.05 + (Column.getWidth() + Column.getGap()) * i,
             unsortedArray[i],
             unsortedArray.length
-        )
+        );
+        column.draw()
+
     }
     return;
 }
 
-function drawColumn(x, value, maxValue){
-    column = new Column(x, value, maxValue);
-    column.draw();
-    return;
-}
-
-function mapNumberRange(number, fromLow, toLow, fromHigh, toHigh){
+function mapNumberRange(number, fromLow, toLow, fromHigh, toHigh) {
     //rule of three considering ranges
     i = toLow - fromLow;
     j = toHigh - fromHigh;
 
-    return number*j/i+fromHigh;
-}
-
-function resetCanvas(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    drawCanvasBackground('#EF2D56');
-
-    function drawCanvasBackground(color){
-        ctx.fillStyle = color;
-        ctx.fillRect(0,0,canvas.width,canvas.height);
-        return;
-    }
-
-    return;
-}
-
-function createCanvas(){
-    let canvas = document.querySelector('canvas');
-    let ratio = Math.ceil(window.devicePixelRatio);
-
-    canvas.width = window.innerWidth * ratio;
-    canvas.height = window.innerHeight * ratio;
-
-    canvas.style.width = window.innerWidth+'px';
-    canvas.style.height = window.innerHeight+'px';
-
-    return canvas;
+    return number * j / i + fromHigh;
 }
