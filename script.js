6dd390bdc375c1a4cc035be4691d6d71ff9484c4
alnnvn <alanvan3791@gmail.com>
2022-12-30 21:17:18 -0300

    column drawing working for n array indexes

---
+++ script.js
@@ -0,0 +1,96 @@
+const canvas = createCanvas();
+const ctx = canvas.getContext('2d');
+
+//#001011
+
+class Column{
+    constructor(x, value){
+        let gapMultiplier = unsortedArray.length/(unsortedArray.length*5)**2
+        this.gap = (canvas.width)*gapMultiplier;
+        this.x = x;
+        this.value = value;
+        this.width = ((canvas.width-(sideMargin*2))-(this.gap*(unsortedArray.length-1)))/unsortedArray.length
+        this.height = mapNumberRange(
+            value,
+            0,
+            unsortedArray.length,
+            canvas.height*0.15, //min column height
+            topMargin //max column height
+        );
+    }
+
+    draw(){
+        console.log(`drawing column at ${this.x}`);
+        ctx.fillStyle = "#FFFFFC";
+        ctx.fillRect(this.x, canvas.height-bottomMargin, this.width, -this.height);
+        return;
+    }
+
+    static getGap(){
+        return new Column().gap;
+    }
+
+    static getWidth(){
+        return new Column().width;
+    }
+}
+var sideMargin = canvas.width*0.05;
+var topMargin = canvas.height*0.95;
+var bottomMargin = canvas.height*0.02;
+var unsortedArray = createUnsortedArray(10);
+
+drawCanvasBackground('#EF2D56');
+
+drawGraph();
+
+//FUNCTIONS
+function createUnsortedArray(size){
+    let array = [];
+    for(let i = 0; i<size; i++){
+        array.push(i);
+    }
+    return array;
+}
+
+function drawGraph(){
+    for(let i = 0; i<unsortedArray.length; i++){
+        drawColumn(
+            canvas.width*0.05+(Column.getWidth()+Column.getGap())*i,
+            unsortedArray[i],
+            unsortedArray.length
+        )
+    }
+    return;
+}
+
+function drawColumn(x, value, maxValue){
+    cleiton = new Column(x, value, maxValue);
+    cleiton.draw();
+    return;
+}
+
+function mapNumberRange(number, fromLow, toLow, fromHigh, toHigh){
+    i = toLow - fromLow;
+    j = toHigh - fromHigh;
+
+    return number*j/i+fromHigh;
+}
+
+function drawCanvasBackground(color){
+    ctx.fillStyle = color;
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    return;
+}
+
+function createCanvas(){
+    let canvas = document.querySelector('canvas');
+    ratio = window.devicePixelRatio;
+
+    canvas.width = window.innerWidth * ratio;
+    canvas.height = window.innerHeight * ratio;
+
+    canvas.style.width = window.innerWidth;
+    canvas.style.height = window.innerHeight;
+
+    return canvas;
+}
