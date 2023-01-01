bec74789e7dd44a8e83a734808f915a7f4008811
alnnvn <alanvan3791@gmail.com>
2022-12-31 22:19:48 -0300

    selection sort working properly with any array size

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-/*
-const canvas = createCanvas();
-const CTX = canvas.getContext('2d');
-
-//#001011*/
 class Settings {
     constructor(method, order, speed, quantity) {
         this.method = method;
@@ -12,8 +7,66 @@ class Settings {
     }
 
     switchSortMethod() {
-        return new BubbleSort(this.order);
+        if (this.method === 'bubblesort') {
+            return new BubbleSort(this.order, unsortedArray.access);
+        }
+        else if (this.method === 'selectionsort') {
+            return new SelectionSort(this.order, unsortedArray.access);
+        }
+
     }
+}
+
+class uArray {
+    constructor(size) {
+        this.size = size;
+        this.access = uArray.createUnsortedArray(this.size)
+
+    }
+
+    static createUnsortedArray(size) {
+        let array = [];
+
+        fillArray(array);
+        shuffleArray(array);
+
+        function shuffleArray(array) {
+            for (let i = 0; i < size * 2; i++) {
+                let a = Math.floor(Math.random() * size);
+                let b = Math.floor(Math.random() * size);
+                let c;
+                c = array[a];
+                array[a] = array[b];
+                array[b] = c;
+            }
+        }
+
+        function fillArray(array) {
+            for (let i = 0; i < size; i++) {
+                array.push(i);
+            }
+        }
+
+        /*USING SETS - complexity directly dependent on Math.random(), 
+        which may generate instances with a overwhelming amount of loops.
+        Usage limited to small array sizes
+        while(array.length != size){
+            let num = Math.floor(Math.random()*size+1);
+            array.push(num)
+    
+            array.forEach(i=>{
+                array.forEach(j=>{
+                    if(i == j){
+                        let set = new Set(array);
+                        array = Array.from(set);
+                    }
+                })
+            })
+        }  */
+        return array;
+    }
+
+
 }
 
 class Canvas {
@@ -88,15 +141,15 @@ class Canvas {
 
 class Column {
     constructor(xPos, value) {
-        let gapMultiplier = unsortedArray.length / (unsortedArray.length * 5) ** 2
+        let gapMultiplier = unsortedArray.access.length / (unsortedArray.access.length * 5) ** 2
         this.gap = (canvas.width) * gapMultiplier;
         this.x = xPos;
         this.value = value;
-        this.width = ((canvas.width - (canvas.sideMargin * 2)) - (this.gap * (unsortedArray.length - 1))) / unsortedArray.length
+        this.width = ((canvas.width - (canvas.sideMargin * 2)) - (this.gap * (unsortedArray.access.length - 1))) / unsortedArray.access.length
         this.height = mapNumberRange(
             value,
             0,
-            unsortedArray.length,
+            unsortedArray.access.length,
             canvas.height * 0.15, //min column height
             canvas.topMargin //max column height
         );
@@ -127,10 +180,12 @@ class Column {
 }
 
 class BubbleSort {
-    constructor(order) {
+    constructor(order, array) {
         this.order = order
         this.i = 0;
         this.isSortedCounter = 0;
+        this.isSorting = true;
+        this.array = array;
     }
 
     sort() {
@@ -145,21 +200,21 @@ class BubbleSort {
     }
 
     increasing() {
-        this.sortingAlgorithm(unsortedArray[this.i] > unsortedArray[this.i + 1]);
+        this.sortingAlgorithm(this.array[this.i] > this.array[this.i + 1]);
         return;
     }
 
     decreasing() {
-        this.sortingAlgorithm(unsortedArray[this.i] < unsortedArray[this.i + 1]);
+        this.sortingAlgorithm(this.array[this.i] < this.array[this.i + 1]);
         return;
     }
 
     sortingAlgorithm(sortCondition) {
-        //sort increasing
+
         if (sortCondition) {
-            let c = unsortedArray[this.i];
-            unsortedArray[this.i] = unsortedArray[this.i + 1];
-            unsortedArray[this.i + 1] = c;
+            let c = this.array[this.i];
+            this.array[this.i] = this.array[this.i + 1];
+            this.array[this.i + 1] = c;
             this.isSortedCounter = 0;
         }
         else {
@@ -167,39 +222,127 @@ class BubbleSort {
         }
 
         //check if its sorted
-        if (this.isSortedCounter >= unsortedArray.length) {
-            isSorting = false;
+        if (this.isSortedCounter >= this.array.length) {
+            this.isSorting = false;
         }
 
         this.i++; //next index
 
         //if loop through the array back from the start
-        if (this.i >= unsortedArray.length - 1) {
+        if (this.i >= this.array.length - 1) {
             this.i = 0;
         }
     }
 }
 
+class SelectionSort {
+    constructor(order, array) {
+        this.array = array;
+        this.order = order
+        this.isSortedCounter = 1;
+        this.isSorting = true;
+
+        this.minMax = {
+            value: this.array[0],
+            index: 0
+        }
+
+        this.k = 0;
+        this.i = 0;
+        this.j = 0;
+    }
+
+    sort() {
+        if (this.order === 'increasing') {
+            this.increasing();
+            return;
+        }
+        else if (this.order === 'decreasing') {
+            this.decreasing();
+            return;
+        }
+    }
+
+    increasing() {
+        this.sortingAlgorithm(
+            this.array[this.i] < this.minMax.value,
+            this.array[this.k] < this.array[this.k + 1]
+        );
+        return;
+    }
+
+    decreasing() {
+        this.sortingAlgorithm(
+            this.array[this.i] > this.minMax.value,
+            this.array[this.k] > this.array[this.k + 1]
+        );
+        return;
+    }
+
+    sortingAlgorithm(sortCondition, checkSortedCondition) {
+        let self = this;
+        /*
+        console.log(`isSortedCounter=${this.isSortedCounter}`)
+        console.log(`I=${this.i}`)
+        console.log(`J=${this.j}`)
+*/
+        checkIsSorted();
+        finMinMaxNumber();   
+        switchPositions();
+        
+
+        //functions
+        function switchPositions() {
+            if (self.i >= self.array.length - 1 && self.j < self.array.length) {
+        
+                //loop i through the array back from the last switched position
+                self.i = self.j + 1;
+
+                //switching positions
+                let c = self.array[self.j];
+                self.array[self.j] = self.array[self.minMax.index];
+                self.array[self.minMax.index] = c;
+
+                //preparing for next loop
+                self.j++;
+                self.minMax.value = self.array[self.j];
+                self.minMax.index = self.j;
+            }
+            else{
+                //next index
+                self.i++; 
+                return;
+            }
+            return;
+        }
+
+        function finMinMaxNumber() {
+            if (sortCondition) {
+                self.minMax.value = self.array[self.i];
+                self.minMax.index = self.i;
+            }
+        }
+
+        function checkIsSorted() {
+            if(self.j >= self.array.length){
+                self.isSorting = false;
+                console.log('finished sorting')
+            }
+        }
+    }
+}
 
 //global variables
-
 const canvas = new Canvas('#EF2D56');
 const CTX = canvas.context;
-const sortSettings = new Settings('bubblesort', 'increasing', 0, 3000);
-
-var unsortedArray;
-var isSorting = true;
-var unsortedArraySize = 1000;
-
-
+const sortSettings = new Settings('bubblesort', 'decreasing', 0, 3000);
+var unsortedArray = new uArray(1000);
 
 start();
 
-
-
 //FUNCTIONS
 function start() {
-    unsortedArray = createUnsortedArray(unsortedArraySize);
+
     updateCanvas();
     return;
 }
@@ -216,7 +359,7 @@ function sortNumberArray() {
     let sortMethod = sortSettings.switchSortMethod();
     var loop = function () { //infinite loop
         let timeout = setTimeout(() => { //delay between operations
-            if (isSorting) {
+            if (sortMethod.isSorting) {
                 //define comparisons per operation
                 for (let k = 0; k < sortSettings.quantity; k++) {
                     sortMethod.sort();
@@ -233,61 +376,19 @@ function sortNumberArray() {
         }, sortSettings.speed); //delay value
     }
 
-    if (isSorting) {
+    if (sortMethod.isSorting) {
         window.requestAnimationFrame(loop);
     }
 
     return;
 }
 
-function createUnsortedArray(size) {
-    let array = [];
-
-    fillArray(array);
-    shuffleArray(array);
-
-    function shuffleArray(array) {
-        for (let i = 0; i < size * 2; i++) {
-            let a = Math.floor(Math.random() * size);
-            let b = Math.floor(Math.random() * size);
-            let c;
-            c = array[a];
-            array[a] = array[b];
-            array[b] = c;
-        }
-    }
-
-    function fillArray(array) {
-        for (let i = 0; i < size; i++) {
-            array.push(i);
-        }
-    }
-
-    /*USING SETS - complexity directly dependent on Math.random(), 
-    which may generate instances with a overwhelming amount of loops.
-    Usage limited to small array sizes
-    while(array.length != size){
-        let num = Math.floor(Math.random()*size+1);
-        array.push(num)
-
-        array.forEach(i=>{
-            array.forEach(j=>{
-                if(i == j){
-                    let set = new Set(array);
-                    array = Array.from(set);
-                }
-            })
-        })
-    }  */
-    return array;
-}
-
 function drawGraph() {
-    for (let i = 0; i < unsortedArray.length; i++) {
+    for (let i = 0; i < unsortedArray.access.length; i++) {
         let column = new Column(
             canvas.width * 0.05 + (Column.getWidth() + Column.getGap()) * i,
-            unsortedArray[i],
-            unsortedArray.length
+            unsortedArray.access[i],
+            unsortedArray.access.length
         );
         column.draw()
 
